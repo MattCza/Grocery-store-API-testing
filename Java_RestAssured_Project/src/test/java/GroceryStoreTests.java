b65ee27de6de042d1be60e643b8f438f75475d3e
Mateusz Czajkowski/Modem Verification Support II (SDV) /SRPOL/Professional/Samsung Electronics <m.czajkowsk2@samsung.com>
2025-03-07 12:17:51 +0100

    Java project init

--- Java_RestAssured_Project/src/test/java/GroceryStoreTests.java
+++ Java_RestAssured_Project/src/test/java/GroceryStoreTests.java
@@ -1,13 +1,20 @@
+import io.restassured.path.json.JsonPath;
+import io.restassured.response.Response;
 import org.hamcrest.Matchers;
+import org.testng.Assert;
 import org.testng.annotations.BeforeClass;
 import org.testng.annotations.Test;
 
+import java.util.List;
+import java.util.Map;
+
 import static io.restassured.RestAssured.*;
-import static org.hamcrest.Matchers.equalTo;
-import static org.hamcrest.Matchers.not;
+import static org.hamcrest.Matchers.*;
 
 public class GroceryStoreTests {
 
+    int productID;
+    int replaceProductID;
 
     @BeforeClass
     public void setupEnvironment() {
@@ -44,6 +51,36 @@ public class GroceryStoreTests {
     }
 
     @Test
+    public void getFirstAndFourthElement() {
+        Response response = given()
+                .when()
+                .get("/products");
+
+        JsonPath jsonPath = response.jsonPath();
+
+        List<Map<String, Object>> products = jsonPath.getList("findAll { it.inStock == true }");
+
+        Map<String, Object> product1 = products.get(0);
+        Map<String, Object> product4 = products.get(3);
+
+        productID = (int) product1.get("id");
+        replaceProductID = (int) product4.get("id");
+
+        Assert.assertEquals(productID, 4643);
+        Assert.assertEquals(replaceProductID, 1225);
+    }
+
+    @Test
+    public void getFirstProductFoundInStock() {
+        given()
+                .when()
+                .get("/products")
+                .then()
+                .assertThat()
+                .body("id", hasItem(productID));
+    }
+
+    @Test
     public void getAllProductsResponseTimeLessThan5000mls() {
         given()
                 .when()
@@ -51,6 +88,30 @@ public class GroceryStoreTests {
                 .then()
                 .time(Matchers.lessThan(5000L));
     }
+
+    @Test
+    public void getProductWithID4643byPath() {
+        given()
+                .pathParam("productID", productID)
+                .when()
+                .get("/products/{productID}")
+                .then()
+                .assertThat()
+                .body("id", equalTo(productID));
+    }
+
+    @Test
+    public void getAllProductWithCategoryCoffeeByQuery() {
+        String categoryCoffee = "coffee";
+        given()
+                .queryParam("category", categoryCoffee)
+                .when()
+                .get("/products")
+                .then()
+                .assertThat()
+                .body("category", hasItem("coffee")).log().body();
+    }
+
 
 
 }
